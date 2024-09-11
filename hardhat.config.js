b7dc3c0eb3f324616592eabc43cc797afbdf8179
Bryanmankind <shegeemankind@gmail.com>
2024-09-11 04:06:12 +0100

    update

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,8 +4,8 @@ module.exports = {
   solidity: "0.8.24",
   networks: {
     "scroll-sepolia": {
-      url: "https://scroll-sepolia.blastapi.io/33eaf70b-45fc-43f3-88f2-3801210d2aba",
-      accounts: ["2a442a5aea064e8f9b00ae0069dd857355a9b37682d61e82151118007c3d6b66"],
+      url: process.env.SCROLL_SEPOLIA_URL,
+      accounts: [process.evn.DEPLOYER],
     },
   }
 };
